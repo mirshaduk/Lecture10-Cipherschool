c1aac1bf6d90aac558f4d727b7efc7f7f1926421
mirshaduk <119918613+mirshaduk@users.noreply.github.com>
2024-07-06 10:01:07 +0530

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int i,j,k;
+    i=0;
+    j=0;
+    k=0;
+    cout<<i<<" "<<j<<" "<<k<<endl;
+    j =i++;
+    cout<<i<<" "<<j<<" "<<k<<endl;
+    k=++i;
+
+    // for(i=1;  ;i++)//called the update which happens after the iteration(part of a loop is completed)
+    // {
+    //     //block which gets exicuted
+        
+    
+    //     cout<<i<<" ";
+    j=i++;
+    k=++i;
+
+    
+    }
